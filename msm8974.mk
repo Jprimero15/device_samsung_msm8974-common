4a12d95b1e4569786bf92f68c50cca3b87e9364f
Joshua Primero <jprimero155@gmail.com>
2019-09-21 10:00:41 +0000

    msm8974-common: wifi: name it .hlte and build the interface
    Signed-off-by: Joshua Primero <jprimero155@gmail.com>

--- msm8974.mk
+++ msm8974.mk
@@ -188,7 +188,7 @@ PRODUCT_PACKAGES += \
 
 # Wifi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service \
+    android.hardware.wifi@1.0-service.hlte \
     hostapd \
     wificond \
     wpa_supplicant \
